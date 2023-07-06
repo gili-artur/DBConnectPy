0ce3af9a88677bff7afc4ead86d1a213b30ee50f
Artur Gilimkhanov <666999zzzz@gmail.com>
2023-07-06 18:47:37 +0400

    added old comments

--- hanov/dbconn/serializers.py
+++ hanov/dbconn/serializers.py
@@ -6,3 +6,32 @@ class PeopleSerializer(serializers.ModelSerializer):
     class Meta:
         model = People
         fields = '__all__'
+
+# class PeopleSerializer(serializers.Serializer):
+#     surname = serializers.CharField()
+#     firstname = serializers.CharField()
+#     patronymic = serializers.CharField()
+#     birthdate = serializers.DateField()
+#     city = serializers.CharField()
+#     telephone = serializers.CharField(max_length=11)
+
+# class PeopleSerializer(serializers.Serializer):
+#     surname = serializers.CharField()
+#     firstname = serializers.CharField()
+#     patronymic = serializers.CharField()
+#     birthdate = serializers.DateField()
+#     city = serializers.CharField()
+#     telephone = serializers.CharField(max_length=11)
+#
+#     def create(self, validated_data):
+#         return People.objects.create(**validated_data)
+#
+#     def update(self, instance, validated_data):
+#         instance.surname = validated_data.get("surname", instance.surname)
+#         instance.firstname = validated_data.get("firstname", instance.firstname)
+#         instance.patronymic = validated_data.get("patronymic", instance.patronymic)
+#         instance.birthdate = validated_data.get("birthdate", instance.birthdate)
+#         instance.city = validated_data.get("city", instance.city)
+#         instance.telephone = validated_data.get("telephone", instance.telephone)
+#         instance.save()
+#         return instance
--- hanov/dbconn/views.py
+++ hanov/dbconn/views.py
@@ -6,11 +6,92 @@ from .serializers import PeopleSerializer
 
 # Create your views here.
 
+# html page
 def db_index(request):
     site_users = People.objects.all()
     return render(request, 'dbconn/db_index.html', {'site_users': site_users})
 
 
+# view on serializer
 class PeopleViewSet(viewsets.ModelViewSet):
     queryset = People.objects.all()
     serializer_class = PeopleSerializer
+
+# class PeopleAPIveiw(APIView):
+#     def get(self, request):
+#         userdata = People.objects.all()
+#         return Response({'posts': PeopleSerializer(userdata, many=True).data})
+# class PeopleViewSet(viewsets.ModelViewSet):
+#     queryset = People.objects.all()
+#     serializer_class = PeopleSerializer
+#
+#
+#     def post(self, request):
+#         serializer = PeopleSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         try:
+#             post_new = People.objects.create(
+#                 surname=request.data['surname'],
+#                 firstname=request.data['firstname'],
+#                 patronymic=request.data['patronymic'],
+#                 birthdate=request.data['birthdate'],
+#                 city=request.data['city'],
+#                 telephone=request.data['telephone'],
+#             )
+#
+#             return Response({'post': PeopleSerializer(post_new).data})
+# #
+# #
+#
+# class PeopleAPIList(generics.ListCreateAPIView):
+#     queryset = People.objects.all()
+#     serializer_class = PeopleSerializer
+#
+#
+# class PeopleAPIUpdate(generics.UpdateAPIView):
+#     queryset = People.objects.all()
+#     serializer_class = PeopleSerializer
+#
+#
+# class PeopleDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = People.objects.all()
+#     serializer_class = PeopleSerializer
+#
+#         except IntegrityError as e:
+#             return Response({'Error:': str(e)})
+
+# class PeopleAPIveiw(APIView):
+#     def get(self):
+#         userdata = People.objects.all()
+#         return Response({'posts': PeopleSerializer(userdata, many=True).data})
+#
+#     def post(self, request):
+#         serializer = PeopleSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         try:
+#             serializer.save()
+#             return Response({'post': serializer.data})
+#
+#         except IntegrityError as e:
+#             return Response({'Error:': str(e)})
+#
+#     def put(self, request, *args, **kwargs):
+#         pk = kwargs.get("pk", None)
+#         if not pk:
+#             return Response({"Error": "Method PUT not allowed - PK invalid"})
+#
+#         try:
+#             instance = People.objects.get(pk=pk)
+#         except:
+#             return Response({"Error": "Object doesn't exists"})
+#
+#         serializer = PeopleSerializer(data=request.data, instance=instance)
+#         serializer.is_valid(raise_exception=True)
+#         try:
+#             serializer.save()
+#             return Response({'post': serializer.data})
+#           #error bd
+#         except IntegrityError as e:
+#             return Response({'Error:': str(e)})
+#         # serializer.save()
+# return Response({"posts": serializer.data})
